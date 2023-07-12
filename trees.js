c6c7b1e5a0d270d7b39638172e09984dab18e85f
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-07-12 13:03:48 +1000

    Update trees.js

--- trees.js
+++ trees.js
@@ -2,85 +2,63 @@ const stockItems = [
   {
       name: "Fruit Tree (Apple)",
       price: 25,
-      imgSrc: "path/to/image1.jpg"
-  },
-  {
-      name: "Fruit Tree (Pear)",
-      price: 10,
-      imgSrc: "path/to/image2.jpg"
+      imgSrc: "/images/apple.jpg"
   },
   {
       name: "Fruit Tree (Olive)",
       price: 5,
-      imgSrc: "path/to/image3.jpg"
+      imgSrc: "/images/olive.jpg"
   },
   {
       name: "Shade Tree (Oak)",
       price: 25,
-      imgSrc: "path/to/image3.jpg"
+      imgSrc: "/images/oak.jpg"
   },
   {
       name: "Shade Tree (Maple)",
       price: 25,
-      imgSrc: "path/to/image3.jpg"
+      imgSrc: "/images/maple.jpg"
   },
   {
       name: "Evergreen Tree (Pine)",
       price: 25,
-      imgSrc: "path/to/image3.jpg"
+      imgSrc: "/images/pine.jpg"
   },
   {
       name: "Evergreen Tree (Fir)",
       price: 25,
-      imgSrc: "path/to/image3.jpg"
-  },
-  {
-      name: "Ornamental Tree (Magnolia)",
-      price: 25,
-      imgSrc: "path/to/image3.jpg"
-  },
-  {
-      name: "Flowering Tree (Cherry)",
-      price: 25,
-      imgSrc: "path/to/image3.jpg"
-  },
-  {
-      name: "Conifers Tree (Juniper)",
-      price: 25,
-      imgSrc: "path/to/image3.jpg"
+      imgSrc: "/images/fir.jpg"
   },
 ];
 
 function generateStockItemHTML(item) {
-const inputId = `quantity-${item.name.replace(/\s+/g, '-').toLowerCase()}`;
+  const inputId = `quantity-${item.name.replace(/\s+/g, '-').toLowerCase()}`;
 
-return `
-  <div class="col-md-6">
-    <p>Item: ${item.name}</p>
-    <p>Price: $${item.price}</p>
-    <div class="input-group">
-      <span class="input-group-btn">
-        <button type="button" class="btn btn-default btn-number" data-type="minus" data-field="${inputId}">
-          <span class="glyphicon glyphicon-minus"></span>
-        </button>
-      </span>
-      <input type="text" id="${inputId}" name="quantity" class="form-control input-number" value="1" min="1" max="10">
-      <span class="input-group-btn">
-        <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="${inputId}">
-          <span class="glyphicon glyphicon-plus"></span>
-        </button>
-      </span>
+  return `
+  <div class="stock-item-container">
+  <div class="stock-item">
+    <div class="card">
+      <img src="${item.imgSrc}" alt="${item.name}">
+      <div class="card-body">
+        <h2>${item.name}</h2>
+        <p>Price: $${item.price}</p>
+        <div class="quantity">
+          <a href="#" class="quantity__minus"><span>-</span></a>
+          <input name="quantity" type="text" class="quantity__input" value="1">
+          <a href="#" class="quantity__plus"><span>+</span></a>
+        </div>
+        <br>
+      </div>
     </div>
-    <br>
-    <button class="btn btn-success add-to-cart-button" data-name="${item.name}" data-price="${item.price}" data-field="${inputId}">Add to Cart</button>
   </div>
-`;
+</div>
+  `;
 }
 
 const stockContainer = document.getElementById("trees");
 for (let item of stockItems) {
-const itemHTML = generateStockItemHTML(item);
-stockContainer.insertAdjacentHTML("beforeend", itemHTML);
+  const itemHTML = generateStockItemHTML(item);
+  stockContainer.insertAdjacentHTML("beforeend", itemHTML);
 }
 
 function incrementQuantity(inputId) {
