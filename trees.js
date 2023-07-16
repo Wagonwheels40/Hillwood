b04f803e0e0478da4b844663baf359230e8033c4
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-07-16 15:58:41 +1000

    Update trees.js

--- trees.js
+++ trees.js
@@ -36,25 +36,25 @@ function generateStockItemHTML(item) {
 
   return `
   <div class="stock-item-container">
-  <div class="stock-item">
-    <div class="card">
-      <img src="${item.imgSrc}" alt="${item.name}">
-      <div class="card-body">
-        <h2>${item.name}</h2>
-        <h5>Price: $${item.price}</h5>
-        <div class="quantity">
-          <a href="#" class="quantity__minus"><span>-</span></a>
-          <input name="quantity" type="text" class="quantity__input" value="1">
-          <a href="#" class="quantity__plus"><span>+</span></a>
-        </div>
-        <div class="cart-buttons">
-        <a class="btn btn-primary" data-name="${item.name}" data-price="${item.price}" data-field="${inputId}">Add to Cart</a>
-        </div>
-        <br>
+      <div class="stock-item">
+          <div class="card">
+              <img src="${item.imgSrc}" alt="${item.name}">
+              <div class="card-body">
+                  <h2>${item.name}</h2>
+                  <h5>Price: $${item.price}</h5>
+                  <div class="quantity">
+                      <a href="#" class="quantity__minus" onclick="decrement('${inputId}')"><span>-</span></a>
+                      <input name="quantity" id="${inputId}" type="text" class="quantity__input" value="1">
+                      <a href="#" class="quantity__plus" onclick="increment('${inputId}')"><span>+</span></a>
+                  </div>
+                  <div class="cart-buttons">
+                      <a class="btn btn-primary" data-name="${item.name}" data-price="${item.price}" data-field="${inputId}" onclick="addToCart('${item.name}', ${item.price}, '${inputId}')">Add to Cart</a>
+                  </div>
+                  <br>
+              </div>
+          </div>
       </div>
-    </div>
   </div>
-</div>
   `;
 }
 
@@ -64,33 +64,25 @@ for (let item of stockItems) {
   stockContainer.insertAdjacentHTML("beforeend", itemHTML);
 }
 
-$(document).ready(function() {
-  const minus = $('.quantity__minus');
-  const plus = $('.quantity__plus');
-  const input = $('.quantity__input');
+function increment(inputId) {
+  const inputElement = document.getElementById(inputId);
+  let value = parseInt(inputElement.value);
+  value++;
+  inputElement.value = value;
+}
 
-  minus.click(function(e) {
-    e.preventDefault();
-    var inputElement = $(this).siblings('.quantity__input');
-    var value = parseInt(inputElement.val());
-    if (value > 1) {
+function decrement(inputId) {
+  const inputElement = document.getElementById(inputId);
+  let value = parseInt(inputElement.value);
+  if (value > 1) {
       value--;
-    }
-    inputElement.val(value);
-  });
-
-  plus.click(function(e) {
-    e.preventDefault();
-    var inputElement = $(this).siblings('.quantity__input');
-    var value = parseInt(inputElement.val());
-    value++;
-    inputElement.val(value);
-  });
-});
+  }
+  inputElement.value = value;
+}
 
 function addToCart(itemName, itemPrice, inputId) {
-  const quantityInput = $('#' + inputId);
-  const quantity = parseInt(quantityInput.val());
+  const quantityInput = document.getElementById(inputId);
+  const quantity = parseInt(quantityInput.value);
 
   const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
 
@@ -100,20 +92,25 @@ function addToCart(itemName, itemPrice, inputId) {
 
   localStorage.setItem("cartItems", JSON.stringify(cartItems));
 
-  const cartItemCount = $('#cart-count');
-  cartItemCount.text(cartItems.length);
+  const cartItemCount = document.getElementById('cart-count');
+  cartItemCount.innerText = cartItems.length;
 
   alert(quantity + " item(s) added to cart!");
 
-  quantityInput.val("1");
+  quantityInput.value = "1";
+
+  // Encode the cart items data in the URL and redirect to checkout.html
+  const encodedCartItems = encodeURIComponent(JSON.stringify(cartItems));
+  window.location.href = `checkout.html?cartItems=${encodedCartItems}`;
 }
 
-const addToCartButtons = $('.cart-buttons .btn');
-for (let button of addToCartButtons) {
-  button.addEventListener("click", function() {
-    const itemName = button.getAttribute("data-name");
-    const itemPrice = parseFloat(button.getAttribute("data-price"));
-    const inputId = button.getAttribute("data-field");
-    addToCart(itemName, itemPrice, inputId);
-  });
+function checkout() {
+  const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  // You can proceed to the checkout page with cartItems data or perform further operations as needed.
+  console.log(cartItems);
 }
+
+// Initialize cart item count
+const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+const cartItemCount = document.getElementById('cart-count');
+cartItemCount.innerText = cartItems.length;
